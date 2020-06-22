28e63e0e923d2cce80a8d15d046714cb7d65857c
Rachel Phuong <rphuong@cs.washington.edu>
2020-06-21 19:14:42 -0700

    Update list filter with ignore case

--- src/List.js
+++ src/List.js
@@ -15,15 +15,28 @@ class List extends Component {
         };
     }
 
+    /*
+     * Sets up the initial list of options based on the initial pokemon list
+     */
     componentDidMount() {
         this.setState({options: this.filter("", this.state.pokemon)});
     }
 
+    /*
+     * Returns a list of strings for possible pokemon names to display
+     */
     getPokemon = () => {
         return ["Bulbasaur", "Pikachu", "Meowth", "Mew"];
     };
 
+    /*
+     * Returns a list of <li>s where each one is a pokemon which partially matches the
+     * given query string while ignoring case
+     * @param query (string) partial string to search for pokemon names
+     * @param pokemon (string[]) list of possible pokemon names to search
+     */
     filter = (query, pokemon) => {
+        let queryLower = query.toLowerCase();
         let options = [];
         for (let currPoke of pokemon) {
             if (currPoke.toLowerCase().includes(query)) {
@@ -41,7 +54,7 @@ class List extends Component {
                        onChange={(event) => {
                            let query = event.target.value;
                            this.setState({
-                               options: this.filter(query.toLowerCase(), this.state.pokemon),
+                               options: this.filter(query, this.state.pokemon),
                                query: query
                            });
                        }}/>
