cc608c975c56d41f3fdc794bc0f79add1c1f46bb
Rachel Phuong <rphuong@cs.washington.edu>
2020-06-23 19:48:26 -0700

    Make listReducer rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,29 @@
-import {combineReducers} from "redux";
-import listReducer from './listReducer';
+import {UPDATE_QUERY, FETCH_POKEMON, FILTER_OPTIONS} from '../actions/types';
 
-export default combineReducers({
-    list: listReducer
-})
+const initialState = {
+    query: "",
+    pokemon: [],
+    options: []
+};
+
+export default function (state = initialState, action) {
+    switch (action.type) {
+        case UPDATE_QUERY:
+            return {
+                ...state,
+                query: action.data
+            };
+        case FETCH_POKEMON:
+            return {
+                ...state,
+                pokemon: action.data
+            };
+        case FILTER_OPTIONS:
+            return {
+                ...state,
+                options: action.data
+            };
+        default:
+            return state;
+    }
+}
