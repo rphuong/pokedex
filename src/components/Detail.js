831599a4e494b9f7462f1b88b214a0afe041898c
Rachel Phuong <rphuong@cs.washington.edu>
2020-06-23 20:34:43 -0700

    Initial Detail set up

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,31 +1,64 @@
 import React, {Component} from 'react';
+import {connect} from "react-redux";
 
 class Detail extends Component {
 
-    /*
-     * Constructs a new List component
-     * @param props: properties passed from parent
-     */
-    constructor(props) {
-        super(props);
-        this.state = {
-        };
-        this.pokemonId = props.match.params.pokemon_id;
-    }
+  /*
+   * Constructs a new Detail component
+   * @param props: properties passed from parent
+   */
+  constructor(props) {
+    super(props);
+    this.pokemonId = this.props.match.params.pokemon_id;
+    this.setDetails();
+  }
 
-    /*
-     * Sets up the initial list of options based on the initial pokemon list
-     */
-    componentDidMount() {
+  /*
+   * Stores this pokemon's details if pokemon details cached
+   */
+  setDetails = () => {
+    if (this.props.pokemon && this.props.pokemon.length >= this.pokemonId) {
+      this.pokemonDetails = this.props.pokemon[this.pokemonId - 1];
     }
+  };
 
-    render() {
-        return (
-            <div>
-                Detail view for {this.pokemonId}
-            </div>
-        );
-    }
+  /*
+   * Returns this pokemon's id with 0 fillers to make 3 digits
+   */
+  getThreeDigitId = () => {
+    let id = "00" + this.pokemonId;
+    return id.substring(id.length - 3, id.length);
+  };
+
+  render() {
+    if (!this.pokemonDetails)
+      return null;
+    return (
+      <div>
+        <img src={this.pokemonDetails.sprites.front_default}
+             alt={this.pokemonDetails.name}/>
+        <h1>{this.pokemonDetails.name}</h1>
+        <p>ID# {this.getThreeDigitId()}</p>
+        <ul>
+          {this.pokemonDetails.types.map(type => <li>{type.type.name}</li>)}
+        </ul>
+        <p>height: {this.pokemonDetails.height}</p>
+        <p>weight: {this.pokemonDetails.weight}</p>
+        <h2>Moves</h2>
+        <ul>
+          {this.pokemonDetails.moves.map(move => <li>{move.move.name}</li>)}
+        </ul>
+        <h2>Abilities</h2>
+        <ul>
+          {this.pokemonDetails.abilities.map(ability => <li>{ability.ability.name}</li>)}
+        </ul>
+      </div>
+    );
+  }
 }
 
-export default Detail;
+const mapStateToProps = state => ({
+  pokemon: state.pokemon,
+});
+
+export default connect(mapStateToProps)(Detail);
