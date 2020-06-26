1cc50d40edc50efd2688f722dce28e33122031c9
Rachel Phuong <rphuong@cs.washington.edu>
2020-06-26 16:41:42 -0700

    Updates

---
+++ precache-manifest.ada6383f7d7f3fa182136a4d7e5fe527.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b9a0009a1ea81049d781b33b92acff5",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "cb19656f1c872aa37329",
+    "url": "/pokedex/static/css/main.a850dea4.chunk.css"
+  },
+  {
+    "revision": "dcbb5d533461c1c08e48",
+    "url": "/pokedex/static/js/2.fb5277e0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pokedex/static/js/2.fb5277e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb19656f1c872aa37329",
+    "url": "/pokedex/static/js/main.dbf73d20.chunk.js"
+  },
+  {
+    "revision": "5d5b7317cf43a0b8925c",
+    "url": "/pokedex/static/js/runtime-main.2d94522c.js"
+  },
+  {
+    "revision": "c32edad4bbd37166337f03ff44ac847e",
+    "url": "/pokedex/static/media/searchicon.c32edad4.png"
+  }
+]);
